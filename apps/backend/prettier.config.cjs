a8ae1c1b94d75faa98346834ca24d5cf26d8e9de
zomh <michael.schmid@capsloq.de>
2024-03-15 15:02:59 +0100

    Remove prettier config from package.json add it as cjs

---
+++ apps/backend/prettier.config.cjs
@@ -0,0 +1,17 @@
+
+/** @type {import("prettier").Config} */
+module.exports = {
+    plugins: ["prettier-plugin-packagejson", "prettier-plugin-organize-imports"],
+    /**
+     * Fixes prettier formatting for tsconfig.json files with trailing commas.
+     * @todo Remove this fix after https://github.com/prettier/prettier/issues/15956 is resolved.
+     */
+    overrides: [
+        {
+            files: ["tsconfig.json"],
+            options: {
+                trailingComma: "none",
+            },
+        },
+    ],
+};
